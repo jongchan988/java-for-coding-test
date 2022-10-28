e515da2a19478f822dbc20e2a1f2f29f101eb2ad
jongchan988 <jongchan988@gmail.com>
2022-10-28 14:01:53 +0900

    추가: 스택, 큐 예제

---
+++ src/chapter5/큐_예제.java
@@ -0,0 +1,21 @@
+package chapter5;
+import java.util.*;
+public class 큐_예제 {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+
+        // 삽입(5) - 삽입(2) - 삽입(3) - 삽입(7) - 삭제() - 삽입(1) - 삽입(4) - 삭제
+        q.offer(5);
+        q.offer(2);
+        q.offer(3);
+        q.offer(7);
+        q.poll();
+        q.offer(1);
+        q.offer(4);
+        q.poll();
+
+        while (!q.isEmpty()){
+            System.out.println(q.poll());
+        }
+    }
+}
